3f29e2c46068dd98b78dcb4e8d143a31feb73001
Andrew Chey <73145855+cheyster42@users.noreply.github.com>
2025-03-21 05:01:00 +0000

    Initial CRM script

---
+++ crm.py
@@ -0,0 +1,6 @@
+print("Welcome to Your Personal CRM!")
+name = input("Enter name: ")
+event = input("Enter event (Birthday, Thank You, etc.): ")
+date = input("Enter event date (YYYY-MM-DD): ")
+
+print(f"{name} - {event} scheduled for {date}")
